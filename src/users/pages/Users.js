d5d23791c3ad804cff111712361da0a1e4f3fef8
Ryan McGuiness <ryanmcguiness123@gmail.com>
2023-06-28 21:04:08 -0400

    added user card component

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -3,7 +3,7 @@ import React from 'react'
 import UsersList from '../components/UsersList'
 
 const Users = () => {
-    const USERS = []
+    const USERS = [{id:'u1', name:'Ryan McGuiness', image:'https://static.vecteezy.com/system/resources/previews/005/544/718/original/profile-icon-design-free-vector.jpg', placeCount:3}]
     return (
         <div>
             <h1>Users Works</h1> 
